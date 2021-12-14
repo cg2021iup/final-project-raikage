65f01aaef5b50f7abed71e6e898cbe6713d2bfba
Mukhoram <74299958+Mukhoram@users.noreply.github.com>
2021-12-14 20:14:41 +0700

    Added Stars
    Stars for the background and changes on camera

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ import { GLTFLoader } from './js/GLTFLoader.js';
 let scene, camera, renderer, pointLight, ambientLight;
 let controls;
 
+const radius = 6371;
+
 function init(){
     scene = new THREE.Scene();
 
@@ -12,7 +14,7 @@ function init(){
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    camera = new THREE.PerspectiveCamera(55, window.innerWidth/window.innerHeight, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(55, window.innerWidth/window.innerHeight, 0.1, 1e7);
     camera.position.set(0, 0, 750);
 
     controls = new OrbitControls(camera, renderer.domElement);
@@ -31,6 +33,7 @@ function init(){
     var sunMesh, mercuryMesh;
 
     loader.load('./gltf/0.0_Sun.glb', sun_forge, xhr, error);
+    loader.load('./gltf/1.0_Mercury.glb', mercury_forge, xhr, error);
 
     function sun_forge( gltf ){
         console.log( gltf );
@@ -53,6 +56,53 @@ function init(){
 
     function xhr ( xhr ) { console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' ); }
 	function error ( error ) { console.log( 'An error happened' ); }
+
+    //Stars
+    const r = radius, starsGeometry = [ new THREE.BufferGeometry(), new THREE.BufferGeometry() ];
+
+    const vertices1 = [];
+    const vertices2 = [];
+
+    const vertex = new THREE.Vector3();
+
+    for ( let i = 0; i < 250; i ++ ) {
+        vertex.x = Math.random() * 2 - 1;
+        vertex.y = Math.random() * 2 - 1;
+        vertex.z = Math.random() * 2 - 1;
+        vertex.multiplyScalar( r );
+        vertices1.push( vertex.x, vertex.y, vertex.z );
+    }
+
+    for ( let i = 0; i < 1500; i ++ ) {
+        vertex.x = Math.random() * 2 - 1;
+        vertex.y = Math.random() * 2 - 1;
+        vertex.z = Math.random() * 2 - 1;
+        vertex.multiplyScalar( r );
+        vertices2.push( vertex.x, vertex.y, vertex.z );
+    }
+
+    starsGeometry[ 0 ].setAttribute( 'position', new THREE.Float32BufferAttribute( vertices1, 3 ) );
+    starsGeometry[ 1 ].setAttribute( 'position', new THREE.Float32BufferAttribute( vertices2, 3 ) );
+
+    const starsMaterials = [
+        new THREE.PointsMaterial( { color: 0x555555, size: 2, sizeAttenuation: false } ),
+        new THREE.PointsMaterial( { color: 0x555555, size: 1, sizeAttenuation: false } ),
+        new THREE.PointsMaterial( { color: 0x333333, size: 2, sizeAttenuation: false } ),
+        new THREE.PointsMaterial( { color: 0x3a3a3a, size: 1, sizeAttenuation: false } ),
+        new THREE.PointsMaterial( { color: 0x1a1a1a, size: 2, sizeAttenuation: false } ),
+        new THREE.PointsMaterial( { color: 0x1a1a1a, size: 1, sizeAttenuation: false } )
+    ];
+
+    for ( let i = 10; i < 30; i ++ ) {
+        const stars = new THREE.Points( starsGeometry[ i % 2 ], starsMaterials[ i % 6 ] );
+        stars.rotation.x = Math.random() * 6;
+        stars.rotation.y = Math.random() * 6;
+        stars.rotation.z = Math.random() * 6;
+        stars.scale.setScalar( i * 10 );
+        stars.matrixAutoUpdate = false;
+        stars.updateMatrix();
+        scene.add( stars );
+    }
 
     animate();
 }
