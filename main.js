39e61a19cd2a4f4afa6f668a8d69caab27d19045
Rafi Akbar <rafiakbarrafsanjani51@gmail.com>
2021-12-14 20:07:58 +0700

    Added GLTF Loader
    Tried to add the sun and mercury with GLTF Loader, added some GLTF model

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import * as THREE from './build/three.module.js';
 import { OrbitControls } from './js/OrbitControls.js';
+import { GLTFLoader } from './js/GLTFLoader.js';
 
 let scene, camera, renderer, pointLight, ambientLight;
-let controls, cube;
+let controls;
 
 function init(){
     scene = new THREE.Scene();
@@ -11,8 +12,8 @@ function init(){
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 0, 25);
+    camera = new THREE.PerspectiveCamera(55, window.innerWidth/window.innerHeight, 0.1, 1000);
+    camera.position.set(0, 0, 750);
 
     controls = new OrbitControls(camera, renderer.domElement);
     
@@ -24,6 +25,34 @@ function init(){
     });
     scene.add(pointLight, ambientLight);
     pointLight.position.set (0, 0, 0)
+
+    //Planets
+    var loader = new GLTFLoader();
+    var sunMesh, mercuryMesh;
+
+    loader.load('./gltf/0.0_Sun.glb', sun_forge, xhr, error);
+
+    function sun_forge( gltf ){
+        console.log( gltf );
+        sunMesh = gltf.scene;
+        sunMesh.scale.set( 0.4, 0.4, 0.4 );
+        sunMesh.position.set( 0, 0, 0 );
+        sunMesh.castShadow = true;
+        sunMesh.receiveShadow = true;
+        scene.add( sunMesh );
+    }
+    function mercury_forge( gltf ){
+        console.log( gltf );
+        mercuryMesh = gltf.scene;
+        mercuryMesh.scale.set( 0.003, 0.003, 0.003 );
+        mercuryMesh.position.set( 240, 0, 0 );
+        mercuryMesh.castShadow = true;
+        mercuryMesh.receiveShadow = true;
+        scene.add( mercuryMesh );
+    }
+
+    function xhr ( xhr ) { console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' ); }
+	function error ( error ) { console.log( 'An error happened' ); }
 
     animate();
 }
